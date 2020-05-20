d15ae1825953b387fb297209cf51a100d7d193ac
georgina <georginachew@hotmail.sg>
2020-05-20 17:57:50 +0800

    fixed up routing to home and staff home

--- routes/main.js
+++ routes/main.js
@@ -1,1 +1,10 @@
 // main page routing goes here (i.e. home page, catalogue, etc.)
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('home')
+});
+
+module.exports = router;
--- routes/staff.js
+++ routes/staff.js
@@ -1,1 +1,10 @@
 // staff side routing goes here
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/home', (req, res) => {
+    res.render('staff/staffhome')
+});
+
+module.exports = router;

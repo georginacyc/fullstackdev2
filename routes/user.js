8abfacfdde60e5b0eb5e202c8bb523620743a9c4
Codylol <cody.tong01@gmail.com>
2020-05-24 17:12:13 +0800

    Added handlers for user and navbar

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,10 @@ const passport = require('passport');
 
 // routing goes in between !!!!
 
+// Register User
+router.get('/showlogin', (req, res) => {
+	const title = 'Video Jotter';
+	res.render('User/login') // renders views/login.handlebars
+});
+
 module.exports = router;
